42d11aa9e1cdd3621ba726b470788e711c43e4fd
francodev <aaronfranco17@gmail.com>
2018-09-05 15:50:19 -0500

    fixed file structure

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
 	"net/http"
 
-	"github.com/francodev/golang-auth0-example/todo"
+	"github.com/francodev/todo"
 	"github.com/gin-gonic/gin"
 )
 
@@ -23,3 +26,43 @@ func AddTodoHandler(c *gin.Context) {
 }
 
 //DeleteTodoHandler will delete a spcified todo based on user http input
+func DeleteTodoHandler(c *gin.Context) {
+	todoID := c.Param("id")
+	if err := todo.Delete(todoID); err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, "")
+}
+
+// CompleteTodoHandler will complete a specified todo based on user http input
+func CompleteTodoHandler(c *gin.Context) {
+	todoItem, statusCode, err := convertHTTPBodyToTodo(c.Request.Body)
+	if err != nil {
+		c.JSON(statusCode, err)
+		return
+	}
+	if todo.Complete(todoItem.ID) != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, "")
+}
+
+func convertHTTPBodyToTodo(httpBody io.ReadCloser) (todo.Todo, int, error) {
+	body, err := ioutil.ReadAll(httpBody)
+	if err != nil {
+		return todo.Todo{}, http.StatusInternalServerError, err
+	}
+	defer httpBody.Close()
+	return convertJSONBodyToTodo(body)
+}
+
+func convertJSONBodyToTodo(jsonBody []byte) (todo.Todo, int, error) {
+	var todoItem todo.Todo
+	err := json.Unmarshal(jsonBody, &todoItem)
+	if err != nil {
+		return todo.Todo{}, http.StatusBadRequest, err
+	}
+	return todoItem, http.StatusOK, nil
+}
