9b7baa4fb3a6a52d3020615ad0abe2862b9b29e2
francodev <aaronfranco17@gmail.com>
2018-09-05 16:49:31 -0500

    fixed auth in main.go

--- main.go
+++ main.go
@@ -48,38 +48,35 @@ func main() {
         panic(err)
     }
 
-	err := r.Run(":3000")
-	if err != nil {
-		panic(err)
-	}
-	func setAuth0Variables() {
-		audience = os.Getenv("AUTH0_API_IDENTIFIER")
-		domain = os.Getenv("AUTH0_DOMAIN")
-	}
-	
-	// ValidateRequest will verify that a token received from an http request
-	// is valid and signyed by Auth0
-	func authRequired() gin.HandlerFunc {
-		return func(c *gin.Context) {
-	
-			var auth0Domain = "https://" + domain + "/"
-			client := auth0.NewJWKClient(auth0.JWKClientOptions{URI: auth0Domain + ".well-known/jwks.json"}, nil)
-			configuration := auth0.NewConfiguration(client, []string{audience}, auth0Domain, jose.RS256)
-			validator := auth0.NewValidator(configuration, nil)
-	
-			_, err := validator.ValidateRequest(c.Request)
-	
-			if err != nil {
-				log.Println(err)
-				terminateWithError(http.StatusUnauthorized, "token is not valid", c)
-				return
-			}
-			c.Next()
+}
+func setAuth0Variables() {
+	audience = os.Getenv("AUTH0_API_IDENTIFIER")
+	domain = os.Getenv("AUTH0_DOMAIN")
+}
+
+// ValidateRequest will verify that a token received from an http request
+// is valid and signy by authority
+func authRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		var auth0Domain = "https://" + domain + "/"
+		client := auth0.NewJWKClient(auth0.JWKClientOptions{URI: auth0Domain + ".well-known/jwks.json"}, nil)
+		configuration := auth0.NewConfiguration(client, []string{audience}, auth0Domain, jose.RS256)
+		validator := auth0.NewValidator(configuration, nil)
+
+		_, err := validator.ValidateRequest(c.Request)
+
+		if err != nil {
+			log.Println(err)
+			terminateWithError(http.StatusUnauthorized, "token is not valid", c)
+			return
 		}
-	}
-	
-	func terminateWithError(statusCode int, message string, c *gin.Context) {
-		c.JSON(statusCode, gin.H{"error": message})
-		c.Abort()
+		c.Next()
 	}
 }
+
+func terminateWithError(statusCode int, message string, c *gin.Context) {
+	c.JSON(statusCode, gin.H{"error": message})
+	c.Abort()
+}
+
